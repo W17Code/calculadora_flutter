5d616516b85b1dea59be1ea06ef9d78db8f8048b
W17Code <zambranoalex814@gmail.com>
2025-11-21 20:38:01 -0500

    Actualiza la página principal para mostrar la pantalla donde se vera el resultado y las operaciones

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -6,8 +6,26 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Page')),
-      body: const Center(child: Text('Welcome to the Home Page!')),
+      appBar: AppBar(title: const Text('Calculadora Flutter')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(20),
+              color: Colors.grey[200],
+              child: const Text(
+                '0',
+                style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.right,
+              ),
+            ),
+          ),
+          // Aquí puedes añadir los botones debajo
+        ],
+      ),
     );
   }
 }

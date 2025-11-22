4c04c49679558ef5bf53f618e60636772c922bd1
W17Code <zambranoalex814@gmail.com>
2025-11-21 20:46:21 -0500

    Se agregan los botones X y %

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -98,6 +98,19 @@ class MyHomePage extends StatelessWidget {
               ),
             ],
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('×', style: TextStyle(fontSize: 24)),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('÷', style: TextStyle(fontSize: 24)),
+              ),
+            ],
+          ),
         ],
       ),
     );
